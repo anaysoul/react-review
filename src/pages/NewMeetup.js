b500df391ea6dceb423b759c505f2460ff5cc882
anaysoul <soulyana_lakew@outlook.com>
2022-04-25 17:18:49 -0400

    Navigating Programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupsPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       'https://react-review-6c3ad-default-rtdb.firebaseio.com/meetups.json',
@@ -11,7 +15,9 @@ function NewMeetupsPage() {
           'Content-Type': 'application/json',
         },
       }
-    );
+    ).then(() => {
+      history.replace('/');
+    });
   }
 
   return (
